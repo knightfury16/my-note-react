38d060dac98022d2c908a9b4a697f520e0f1067d
knightfury16 <suhaibknight@gmail.com>
2022-07-29 18:06:33 +0600

    Added a back button.

--- src/routes/note.tsx
+++ src/routes/note.tsx
@@ -1,5 +1,6 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
-import { useLocation } from 'react-router-dom';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { Note as SingleNote } from '../components/NoteApp';
 import { Wrapper } from '../utils/Wrapper';
 
@@ -9,10 +10,14 @@ type State = {
 
 export const Note: React.FC = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { state } = location;
   return (
     <Wrapper>
       <Box bg={'beige'} rounded={'5px'} p={'15px'} minH={'xs'}>
+        <Button mb={'12px'} colorScheme={'facebook'} size={'sm'} onClick={() => navigate('/')}>
+          <ArrowBackIcon />
+        </Button>
         <Heading size={'lg'}>{(state as State).note.title}</Heading>
         <Text mt={'10px'}>
           {(state as State).note.body === '' ? 'empty' : (state as State).note.body}{' '}
